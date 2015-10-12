fbf9b40886fb933f0a0c7759592835be2e4e726b
Brian Arsuaga <brian.arsuaga@gmail.com>
2015-10-12 12:37:00 -0500

    kerning the other direction, maximum bold, bigger example

--- eyebook/eyebook.js
+++ eyebook/eyebook.js
@@ -17,8 +17,8 @@ EyebookPage = Ice.$extend('EyebookPage', {
 
 		self.lines = ko.observableArray([]);
 
-		self.lines.push(new EyebookLine('LordHamster', 'Sup d00ds.'));
-		self.lines.push(new EyebookLine('CharlsNChrg', "Don't d00d me."));
+		//self.lines.push(new EyebookLine('LordHamster', 'Sup d00ds.'));
+		//self.lines.push(new EyebookLine('CharlsNChrg', "Don't d00d me."));
 	},
 	insert_line: function(pos) {
 		var self = this;
@@ -124,6 +124,36 @@ EyebookApp = Ice.$extend('EyebookApp', {
 
 $(function() {
 	window.eyebook_app = EyebookApp();
+	function il(n, t) {
+		eyebook_app.pages()[eyebook_app.pages().length-1].lines.push(new EyebookLine(n, t));
+	}
+	il('LordHamster', 'So is that your offer?  full reimbursement for our lost cities and troops if we let her go?');
+	il("CharlsNChrg", "That's an offer.  How much is it, exactly?");
+	il("LordHamster", "Bracer says $221,540 if we get our remaining prisoners back.");
+	il("CharlsNChrg", "I can't make deals for Faq, but I'll make sure she lets your people go.");
+	il("LordHamster", "And the figure doesn't bother you.");
+	il("CharlsNChrg", "It's steep, but I can pay it.");
+	il("LordHamster", "Okay, \"my people\" are saying nfw.");
+	il("LordHamster", "And I'm thinking no, too.  Just because this is something you want.");
+	il("CharlsNChrg", "*sigh*");
+	il("CharlsNChrg", "Parson...I want her for reasons that don't have anything to do with you.");
+	il("CharlsNChrg", "If that offer doesn't interest you, then name something that does.");
+	il("LordHamster", "How bout you give up the calculations I owe you?  All of them.");
+	il("CharlsNChrg", "Done.");
+	il("LordHamster", "And tell me what Jojo's spell does.");
+	il("CharlsNChrg", "I already have.  It sends you home.");
+	il("LordHamster", "No, I want a full technical explanation.");
+	il("LordHamster", "Who created it, why, and what are its specifications?  Its \"underpinnings.\".");
+	eyebook_app.pages.push(new EyebookPage());
+
+	il("CharlsNChrg", "Dione.  What else?");
+	il("LordHamster", "Get us our Archon prisoner back from Jetstone.");
+	il("CharlsNChrg", "I said I can't make deals for other sides.  Jetstone won't even talk to me.  Name something else.");
+	il("LordHamster", "Tell me why you want the Queen so bad.");
+	il("LordHamster", "I mean, you're not even saying \"take her prisoner and hand her over,\" you still want her to be running her side.  Why?");
+	il("CharlsNChrg", "To fulfill another contractual obligation that doesn't concern you.");
+
+
 
 	ko.applyBindings({app: window.eyebook_app}, $('.eyebook_app')[0]);
 });
